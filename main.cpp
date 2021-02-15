7af8c659d393117076e5155cd5ff9f52c58d83c8
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2021-02-15 10:37:18 +0300

    Take out experiments in separate functions

--- main.cpp
+++ main.cpp
@@ -14,25 +14,30 @@ bool sort_function (std::vector<double> first, std::vector<double> second)
     return (first[0]<second[0]);
 }
 
-int main()
+template <typename T>
+std::vector<std::vector<double>> doExperiments(int testsNumber = 10)
 {
-//    srand(time(NULL));
+    int maxVertices = 200;
+    int stepVertices = 10;
+    int maxCapacity = 200;
+    int stepCapacity = 10;
 
-    Network<int> network;
+    Network<T> network;
 
     int k = 0;
     std::vector<std::vector<double>> results;
     results.resize(k+1);
-    for (int i = 20; i <= 200; i += 20)
+    for (int i = 20; i <= maxVertices; i += stepVertices)
     {
-        for (int j = 20; j <= 2000; j += 200)
+        for (int j = 20; j <= maxCapacity; j += stepCapacity)
         {
             network = Network<int>::generateNetwork(i, j);
 
             int edges = network.Edges();
             int maxFlow = 0;
+
             auto start = std::chrono::high_resolution_clock::now();
-            for (int l = 0; l < 10; ++l)
+            for (int l = 0; l < testsNumber; ++l)
             {
                 maxFlow = network.FordFulkersonMaxFlow();
             }
@@ -53,8 +58,13 @@ int main()
         }
     }
 
-    std::sort(results.begin(), results.end(),sort_function);
+    std::sort(results.begin(), results.end(), sort_function);
 
+    return results;
+}
+
+void printResults(std::vector<std::vector<double>> results)
+{
     std::ofstream graphTable("graphTable.csv");
     std::ofstream table("table.csv");
 
@@ -74,15 +84,21 @@ int main()
     }
     graphTable.close();
     table.close();
+}
 
+int main()
+{
     int vertices = 10;
     int capacity = 20;
+    Network<int> network = Network<int>::generateNetwork(vertices, capacity);
 
-//    network = Network<int>::generateNetwork(vertices, capacity);
-//    std::cout << network << std::endl;
-//    network.printForTest();
+    std::cout << network << std::endl;
+//    network.printForTest(); // to test with an implementation found on the internet
 
-//    std::cout << "Max flow = " << network.FordFulkersonMaxFlow();
+    std::cout << "Max flow = " << network.FordFulkersonMaxFlow();
+
+    // Tests
+//    printResults(doExperiments<int>());
 
     return 0;
 }
